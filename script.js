d3d6a51d759c2ba656aab27c033ce4a4f84281ac
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-05-05 21:02:29 +0530

    Update ProgessBar

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let audio = new Audio('http://localhost:5500/Audios/Pushpaa.mp3');
     let volumeControl = document.getElementById('volume');
     let muteButton = document.getElementById('mute');
+    let progressBar = document.getElementById("progressBar");
 
     document.getElementById("play").addEventListener("click", function() {
         audio.play();
@@ -13,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelector("#reset").addEventListener("click",function() {
         audio.currentTime = 0;
+        updateProgressBar();
     });
 
     volumeControl.addEventListener('input', function() {
@@ -23,4 +25,16 @@ document.addEventListener('DOMContentLoaded', function() {
         audio.muted = !audio.muted;
         this.textContent = audio.muted ? 'Unmute' : 'Mute';
     });
+
+
+    audio.addEventListener("timeupdate", function() {
+        updateProgressBar();
+    });
+
+    function updateProgressBar() {
+        if (audio.duration) {
+            var percentage = (audio.currentTime / audio.duration) * 100;
+            progressBar.style.width = percentage + '%';
+        }
+    }
 });
